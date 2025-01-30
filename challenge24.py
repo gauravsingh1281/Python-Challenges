89ee9c36b272122892972191eaf456d080c707a4
Gaurav Pratap Singh <gs492979@gmail.com>
2025-01-30 23:57:28 +0530

    Alter solution added.

--- challenge24.py
+++ challenge24.py
@@ -4,10 +4,25 @@ year = int(input("Enter Year"))
 
 
 def leapYearCheck(year):
-    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
-        print(f"{year} It is a leap year")
+    # 1st way
+
+    #     if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+    #         print(f"{year} It is a Leap Year")
+    #     else:
+    #         print(f"{year} It is not a Leap Year")
+
+    # 2nd way
+
+    if year % 4 == 0:
+        if year % 100 != 0:
+            print(f"{year} It is a Leap Year")
+        else:
+            if year % 400 == 0:
+                print(f"{year} It is a Leap Year")
+            else:
+                print(f"{year} It is not a Leap Year")
     else:
-        print(f"{year} It is not a leap year")
+        print(f"{year} It is not a Leap Year")
 
 
 leapYearCheck(year)
